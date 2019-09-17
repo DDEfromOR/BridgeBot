3ef97dc554ac4015fd6355d6dca34feb26e5abe7
Daniel Evans <dev@microsoft.com>
2019-09-17 14:18:12 -0700

    Add project files.

---
+++ Bots/BridgeBot.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.StreamingExtensions;
+using Microsoft.Bot.StreamingExtensions.Transport.NamedPipes;
+using Microsoft.Bot.StreamingExtensions.Transport.WebSockets;
+
+namespace Microsoft.Bot.Builder.BridgeBot
+{
+    public class BridgeBot : ActivityHandler
+    {
+        private static WebSocketClient _webSocketClient;
+        private static NamedPipeClient _namedPipeClient;
+        private static Dictionary<string, ITurnContext> _turnContexts = new Dictionary<string, ITurnContext>();
+        private BridgeRequestHandler _handler;
+        private bool _useNamedPipes = false;
+        private bool _useWebSockets = true;
+
+        public BridgeBot()
+        {
+            _handler = new BridgeRequestHandler((activity) =>
+            {
+                if (_turnContexts.TryGetValue(activity.Conversation.Id, out var value))
+                {
+                    value.SendActivityAsync(activity).Wait();
+                }
+            });
+
+            // Named Pipe bots do not require authentication, as a named pipe connection must originate from the same 
+            // machine/instance as the bot itself.
+            this._useNamedPipes = false; // Set to true to connect to a named pipe bot.
+            this._useWebSockets = true; // Set to true to connect to WebSocket bot.
+
+            if (!this._useWebSockets && !this._useNamedPipes)
+            {
+                // No bot has been set to bridge to, so there is nothing for BridgeBot to do.
+
+                return;
+            }
+
+            if (this._useWebSockets && this._useNamedPipes)
+            {
+                // This version of BridgeBot can only bridge to one bot at this point, so one of the above should be false;
+
+                return;
+            }
+
+            if (this._useNamedPipes)
+            {
+                SetupNamedPipeConnection();
+            }
+            if (this._useWebSockets)
+            {
+                SetupWebSocketConnection("REPLACE WITH BOT ID", "REPLACE WITH BOT PASSWORD");
+            }
+        }
+
+        private void SetupWebSocketConnection(string botId, string botPassword)
+        {
+            if (!_useWebSockets)
+            {
+                return;
+            }
+            Connector.Authentication.MicrosoftAppCredentials appCredentials = new Connector.Authentication.MicrosoftAppCredentials(botId, botPassword);
+            var authHeaders = new Dictionary<string, string>() { { "authorization", $"Bearer {appCredentials.GetTokenAsync().Result}" }, { "channelid", "emulator" } };
+            _webSocketClient = new WebSocketClient("ws://localhost:3978/api/messages", _handler, null);
+            _webSocketClient.ConnectAsync(authHeaders).Wait();
+        }
+
+        private void SetupNamedPipeConnection()
+        {
+            if (!_useNamedPipes)
+            {
+                return;
+            }
+            _namedPipeClient = new NamedPipeClient("bfv4.pipes", _handler);
+            _namedPipeClient.ConnectAsync();
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            //proxy the message over to the streaming bot
+            var request = new StreamingRequest()
+            {
+                Verb = "POST",
+                Path = "/api/messages"
+            };
+            request.SetBody(turnContext.Activity);
+
+            if (_useNamedPipes)
+            {
+                await _namedPipeClient.SendAsync(request).ConfigureAwait(false);
+            }
+
+            if (_useWebSockets)
+            {
+                await _webSocketClient.SendAsync(request).ConfigureAwait(false);
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    if (!_turnContexts.ContainsKey(turnContext.Activity.Conversation.Id))
+                    {
+                        _turnContexts.TryAdd(turnContext.Activity.Conversation.Id, turnContext);
+                    }
+                    var request = new StreamingRequest()
+                    {
+                        Verb = "POST",
+                        Path = "/api/messages"
+                    };
+                    request.SetBody(turnContext.Activity);
+
+                    if (_useNamedPipes)
+                    {
+                        await _namedPipeClient.SendAsync(request).ConfigureAwait(false);
+                    }
+
+                    if (_useWebSockets)
+                    {
+                        await _webSocketClient.SendAsync(request).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BridgeRequestHandler.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.StreamingExtensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.BridgeBot
+{
+    public class BridgeRequestHandler : RequestHandler
+    {
+        private readonly Action<Activity> _receiveActivities;
+        public BridgeRequestHandler(Action<Activity> receiveActivities)
+        {
+            _receiveActivities = receiveActivities;
+        }
+
+        public override async Task<StreamingResponse> ProcessRequestAsync(ReceiveRequest request, ILogger<RequestHandler> logger = null, object context = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (request.Verb == "POST")
+            {
+                var firstActivity = request.ReadBodyAsJson<Activity>();
+
+                if (request.Streams.Count > 1)
+                {
+                    if (firstActivity == null)
+                    {
+                        throw new InvalidOperationException("Attachment streams received with activity set, but no activities present in activity set.");
+                    }
+
+                    var streamAttachments = new List<Attachment>();
+                    for (int i = 1; i < request.Streams.Count; i++)
+                    {
+                        var stream = request.Streams[i].Stream;
+                        streamAttachments.Add(new Attachment() { ContentType = request.Streams[i].ContentType, Content = request.Streams[i].Stream });
+                    }
+
+                    if (firstActivity.Attachments != null)
+                    {
+                        firstActivity.Attachments = firstActivity.Attachments.Concat(streamAttachments).ToArray();
+                    }
+                    else
+                    {
+                        firstActivity.Attachments = streamAttachments.ToArray();
+                    }
+                }
+
+                _receiveActivities(firstActivity);
+
+                return StreamingResponse.OK();
+            }
+            return StreamingResponse.NotFound();
+        }
+    }
+}
